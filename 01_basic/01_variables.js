a3b66d3b575c7fdac54bab0d4309e309a52c3ec9
1601devanshi <sharmadevanshi0116@gmail.com>
2026-06-12 06:45:23 +0000

    DataTypes changes

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,23 @@
+const accountId = 10234
+let accountEmail = "devanshi@gmail.com"
+var accountPassword  = "84526"
+accountCity = "Jaipur"
+let accountState;
+
+
+// accountId = 2  //not allowed
+
+accountemail ="hc@gmail.com"
+accountpassword = "123654"
+accountCity = "gujrat"
+
+
+
+console.log(accountId);
+
+/*
+prefer not to use Var 
+bcz of issue in block scope and functional scope..
+*/
+
+console.table([accountId, accountemail, accountPassword, accountCity, accountState])
---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all JS code as newer version
+
+//alert(3+3) //usimg node.js not browser
+
+console.log(3+3) // code readability is high
+console.log("Devanshi")
+
+let name = "devi"
+let age = 74
+let isLoggedIn = false
+let state;
+// number => 2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+// null=> std value
+//undefined =>
+    // symbol => unique
+
+
+// object
+
+console.log(typeof age);
+console.log(typeof null); //objext type
+console.log(typeof undefined); //undefine object
