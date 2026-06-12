21feabd76eb52745f64277109838da3f163ed1cc
1601devanshi <sharmadevanshi0116@gmail.com>
2026-06-12 17:36:28 +0000

    changes made

---
+++ 01_basic/05_dataTypesSummary.js
@@ -0,0 +1,21 @@
+// summary
+
+// primitive datat types ; call by values
+// 1 string 2)number 3) boolean 4) null 5) undefined 6)symbol, Bigint
+
+//non primitive call be refrence 
+
+// array , obects , functions
+
+const score = 100
+const scorevalue =100.3
+
+const isLoggedIn = falseconst outsideTemp =null;
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id == anotherId);
+
+const bigNumber = 236598754n;
